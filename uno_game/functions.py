7a0d64198006e1c15d9bea59b18a9dc90dadf913
malharbangdiwala <63497799+malharbangdiwala@users.noreply.github.com>
2020-10-23 10:54:48 +0530

    Update functions.py

--- uno_game/functions.py
+++ uno_game/functions.py
@@ -33,7 +33,6 @@ def create(ob):
         for _ in range(7):
             ob.player_list[j].append(ob.deck1.pop())
 
-
 def set_curr_player(ob, default):
     if ob.current[0] == 'Reverse' and ob.special_check == 0:
         ob.direction_check *= -1
@@ -153,14 +152,17 @@ def bot_action(ob):
                         for _item in ob.player_list[ob.position]:
                             d[_item[1]] += 1
                         new_color = max(d, key=d.get)
+                        print(d,new_color)
                         if new_color == 'Black':
-                            new_color = random.choice(ob.color)
+                            new_color = sorted(d.items(), key = lambda kv:(kv[1], kv[0]))[-2][0]
+                            print(d,new_color)
                     else:
                         new_color = random.choice(ob.color)
                     print("Color changes to:", new_color)
                     ob.message = "%s plays %s %s, new color is %s" % (
                         ob.bot_map[ob.position], item[0], item[1], new_color)
                     ob.current = (ob.current[0], new_color)
+
                 ob.player_list[ob.position].remove(item)
 
                 set_curr_player(ob, False)
@@ -185,8 +187,10 @@ def bot_action(ob):
                         for _item in ob.player_list[ob.position]:
                             d[_item[1]] += 1
                         new_color = max(d, key=d.get)
+                        print(d,new_color)
                         if new_color == 'Black':
-                            new_color = random.choice(ob.color)
+                            new_color = sorted(d.items(), key = lambda kv:(kv[1], kv[0]))[-2][0]
+                            print(d,new_color)
                     else:
                         new_color = random.choice(ob.color)
 
@@ -219,8 +223,10 @@ def bot_action(ob):
                         for _item in ob.player_list[ob.position]:
                             d[_item[1]] += 1
                         new_color = max(d, key=d.get)
+                        print(d,new_color)
                         if new_color == 'Black':
-                            new_color = random.choice(ob.color)
+                            new_color = sorted(d.items(), key = lambda kv:(kv[1], kv[0]))[-2][0]
+                            print(d,new_color)
                     else:
                         new_color = random.choice(ob.color)
                     # new_color = random.choice(ob.color)  # comment this and uncomment previous
